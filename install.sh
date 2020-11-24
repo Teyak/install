8ac5a96b3f1d10c44ef037c764aa7d96f85f003c
Teyak <74811879+Teyak@users.noreply.github.com>
2020-11-24 19:42:42 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $green"sabar yah bro,, agak lama:)"
+echo
+echo $white "INSTALL NANO"
+pkg install nano
+echo $red "INSTALL FIGLET"
+pkg install figlet
+echo $green "INSTALL PYTHON2"
+pkg install python2
+echo $blue "INSTALL COWSAY"
+pkg install cowsay
+echo $yellow "INSTALL RUBY"
+pkg install ruby
+gem install lolcat
+echo $purple "INSTALL TOILET"
+pkg install toilet
+echo $blue "INSTALL PHP"
+pkg install php
